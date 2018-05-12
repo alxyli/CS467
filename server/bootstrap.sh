cf5ad792a3661c44536a8edf236722cda36edc9b
Kevin Lewis <kev.glynne@gmail.com>
2018-05-12 11:36:21 -0400

    Clean up server folder

--- server/bootstrap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export FLASK_APP=./crawler.py
-nohup flask run --host=0.0.0.0
--- server/crwl2.sh
+++
@@ -1,6 +0,0 @@
-# /bin/sh
-<<<<<<< HEAD
-python3.5 crawler2.py
-=======
-python3 crawler2.py
->>>>>>> frontend
